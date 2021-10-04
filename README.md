ba891edc7394c39d71c80012844fa6dc40ec1dc6
Jethro Au <supergalong@gmail.com>
2021-10-04 21:08:33 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # huddle-website
+
+## Welcome! 👋
+
+This is Huddle website. This website is made with ReactJS and styled with styled-components.
+
+## Build with ⚙️
+
+This project is built with ReactJS.
+
+## Screenshots 📷
+
+![](img/screenshot1.png)
+
+## Link 🔗
+
+[Click Me!]()
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
+import { Flex } from "./styles/Flex.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
+
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              This website is build with ReactJS and Styled Components. Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Delectus
+              impedit quis sint minima a.
+            </p>
+            <Button bg="#ff0099" color="#fff">
+              Get Started For Free
+            </Button>
+          </div>
+          <Image src="./images/illustration-mockups.svg" alt=""></Image>
+        </Flex>
+      </Container>
+    </StyledHeader>
+  );
+};
+
+export default Header;
